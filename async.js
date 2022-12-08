68402e5b40364cf34449324f778e8254bf856f0f
fangqijun <fangqijun@codemao.cn>
2022-12-08 18:03:02 +0800

    feat: delayReject

--- async.js
+++ async.js
@@ -2,3 +2,6 @@ export function delay(t) {
     return new Promise(resolve => setTimeout(resolve, t))
 }
 
+export function delayReject(t) {
+    return new Promise((_, reject) => setTimeout(reject, t))
+}
